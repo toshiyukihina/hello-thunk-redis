1f68ede92615ca9391c957fece77a8cc7a1bc665
Toshiyuki Hina <toshiyuki.hina@gmail.com>
2015-05-08 16:00:11 +0900

    Add test case

--- index.js
+++ index.js
@@ -3,65 +3,65 @@
 var redis = require('thunk-redis'),
     Promise = require('bluebird');
 
-// var argv = require('yargs')
-//       .usage('Usage: $0 -x [num] -y [num]')
-//       .demand(['x', 'y'])
-//       .alias('x', 'xxxxx')
-//       .alias('y', 'yyyyy')
-//       .epilog('Copyright 2015')
-//       .argv;
+var argv = require('yargs')
+      .usage('node $0 -t <test>')
+      .option('t', {
+        alias: 'test',
+        demand: true,
+        describe: 'A test function name',
+        type: 'string'
+      })
+      .epilog('Copyright 2015')
+      .argv;
 
-// console.log('(%d, %d)', argv.x, argv.y);
-// console.log(argv._);
-
-// process.exit(0);
-
-var options = {
-  database: 2,
-  usePromise: Promise,
-  debug: true
-};
-
-var nodes = [
-  { host: '127.0.0.1', port: 6379 }
-];
-
-var client = redis.createClient(nodes, options);
-
-var testGetSet = function() {
-  client.on('connect', function() {
-    console.log('Connected!');
-    client.quit();
+var testClusterMode = function() {
+  [true, false].forEach(function(clusterMode) {
+    var client = redis.createClient(7000, {clusterMode: clusterMode});
+    client.on('connect', function() {
+      console.log('* clusterMode=%s', clusterMode);
+      console.dir(client.clientState());
+      client.clientEnd();
+    });
   });
 };
 
-testGetSet();
+var testGetSet = function() {
+  var options = {
+    usePromise: Promise,
+    clusterMode: false,
+    database: 100,
+    debug: true
+  };
 
-// client.on('connect', function() {
-//   console.log('Connected!');
+  var client = redis.createClient(7000, options);
 
-//   client.info().then(function(res) {
-//     console.log('* server info: %s', JSON.stringify(res));
-//     return client.dbsize();
-//   }).then(function(res) {
-//     console.log('* current database size: %s', res);
-//     return client.select(0);
-//   }).then(function(res) {
-//     console.log('* selected database 0: %s', res);
-//     return Promise.all([
-//       client.multi(),
-//       client.set('key', 'redis'),
-//       client.get('key'),
-//       client.exec()
-//     ]);
-//   }).then(function(res) {
-//     console.log('* transactions: %s', res);
-//     return client.quit();
-//   }).then(function(res) {
-//     console.log('* redis client quit: %s', res);
-//   }).catch(function(e) {
-//     console.error(e);
-//   }).finally(function() {
-//     client.clientEnd();
-//   });
-// });
+  client.on('connect', function() {
+    console.dir(client.clientState());
+
+    client.set('key1', 100).then(function(res) {
+      console.log('SET key1 => %s', res);
+      return client.get('key1');
+    }).then(function(res) {
+      console.log('GET key1 => %s', res);
+      return client.set('key2', 200);
+    }).then(function(res) {
+      console.log('SET key2 => %s', res);
+      return client.get('key2');
+    }).then(function(res) {
+      console.log('GET key2 => %s', res);
+      return client.mget(['key1', 'key2']);
+    }).then(function(res) {
+      console.log('MGET key1 key2 => %s', res);
+    }).catch(function(e) {
+      console.error(e);
+    }).finally(function() {
+      console.log('DONE!');
+      client.clientEnd();
+    });
+  });
+};
+
+(function() {
+  console.log('>>> %s', argv.t);
+  eval(argv.t)();
+})();
